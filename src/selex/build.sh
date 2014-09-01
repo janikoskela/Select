315e200bae6fe999807a87a5313baf7a682d2d8f
Jani Koskela <jani.koskela@louhi.fi>
2014-09-01 13:07:21 +0300

    build script

---
+++ src/selex/build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+DESTINATION_FILE_NAME="../dist/Selex.js"
+DESTINATION_MINIFIED_FILE_NAME="../dist/Selex.min.js"
+DESTINATION_MINIFIED_COMPRESSED_FILE_NAME="../dist/Selex.min.js.gz"
+WRAPPER_FILE_NAME="Selex.js"
+TEMP_FILE="temp"
+CWD=$(pwd)
+
+merge() {
+	> "$DESTINATION_FILE_NAME"
+	find ${CWD} -name '*.js' | while read F; do
+	    if [ "${F##*/}" == "$WRAPPER_FILE_NAME" ];
+	    then
+	    	sed '$ d' $F > $TEMP_FILE
+	    	cat $TEMP_FILE >> $DESTINATION_FILE_NAME
+	    	rm $TEMP_FILE
+	    else
+	    	cat "$F" >> $DESTINATION_FILE_NAME
+	    fi
+	done
+	lastLine=$(tac $WRAPPER_FILE_NAME |egrep -m 1 .)
+	echo "$lastLine" >> "$DESTINATION_FILE_NAME"
+}
+
+merge;
+exit 1;
