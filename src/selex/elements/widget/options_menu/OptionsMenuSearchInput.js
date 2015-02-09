ce0fbd4acd35054b532e630476553e771513875e
Jani Koskela <knaitti@gmail.com>
2015-02-09 16:49:32 +0200

    Revert "fix to isHidden prototype. fix for search input behavior"
    This reverts commit 591929571b8a739fee9d1999ca1519e4b6e660f8.

--- src/selex/elements/widget/options_menu/OptionsMenuSearchInput.js
+++ src/selex/elements/widget/options_menu/OptionsMenuSearchInput.js
@@ -3,7 +3,6 @@ SELEX.ELEMENTS.WIDGET.OPTIONS_MENU.OptionsMenuSearchInput = function(Facade) {
 	this.className = "options-menu-search-input";
 	this.tabIndex = -1;
 	this.element;
-	this.allowClose = true;
 
 	this.render = function() {
     	this.element = SELEX.UTILS.createElement(this.type, this.className);
@@ -19,24 +18,19 @@ SELEX.ELEMENTS.WIDGET.OPTIONS_MENU.OptionsMenuSearchInput = function(Facade) {
 		this.value = undefined;
 	}
 
-	this.focusOut = function(e) {
-		if (this.allowClose) {
-			Facade.publish("OptionsMenu:hide");
-			Facade.publish("WidgetWrapper:blur");
-		}
+	this.focusOut = function() {
+		Facade.publish("OptionsMenu:hide");
+		Facade.publish("WidgetWrapper:blur");
 	}
 
 	function onKeyUp(e) {
-		e.preventDefault();
 		e.stopPropagation();
         switch(e.keyCode) {
         	case KEY_CODES.DOWN:
-        		this.allowClose = false;
         		Facade.publish("OptionsMenuList").hoverFirstOption();
         		this.blur();
         		break;
         	default:
-        		this.allowClose = true;
 				var value = this.element.value;
 				if (value.length === 0) {
 					Facade.publish("OptionsMenuList:refresh");
